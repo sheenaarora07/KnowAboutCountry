fbe3db321a2dc0d9b4a3c31c31b110ce45e414bc
Sheena <arorasheena@icloud.com>
2022-02-22 11:00:21 +0530

    Initial Commit

---
+++ KnowAboutCountry/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KnowAboutCountry
+//
+//  Created by Akay Prince on 22/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
